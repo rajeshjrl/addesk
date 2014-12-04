0eecf7094162d737f88b3e7081ed45851739265d
rajeshjrl <rajeshjrl@gmail.com>
2014-12-04 15:10:09 -0500

    scss changes

--- src/main/java/com/cni/addesk/HomeUI.java
+++ src/main/java/com/cni/addesk/HomeUI.java
@@ -6,9 +6,12 @@ import com.vaadin.annotations.PreserveOnRefresh;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.ThemeResource;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.CustomLayout;
+import com.vaadin.ui.MenuBar;
+import com.vaadin.ui.MenuBar.MenuItem;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
@@ -25,10 +28,33 @@ public class HomeUI extends UI
     }
 
     @Override
-    protected void init(VaadinRequest request) {        
+    protected void init(VaadinRequest request) {    
+    	// Define a common menu command for all the menu items.
+    	MenuBar.Command menuCommand = new MenuBar.Command() {
+    	    public void menuSelected(MenuItem selectedItem) {
+    	    	System.out.println("Hello World!");
+    	    }  
+    	};
         final VerticalLayout layout = new VerticalLayout();
         setContent(layout);
         CustomLayout headerTemplate = new CustomLayout("header");
+        MenuBar menubar = new MenuBar();
+        MenuItem menuItem = menubar.addItem("", new ThemeResource("layouts/images/menu_mobile.png"), null);
+        menuItem.addItem("Home", null, menuCommand);
+        menuItem.addItem("Reports", null, menuCommand);
+        menuItem.addItem("History", null, menuCommand);
+        menuItem.addItem("Help & Information", null, menuCommand);
+        menuItem.addItem("Sign Out", null, menuCommand);
+        
+        MenuBar rightMenu = new MenuBar();
+        MenuItem homeMenuItem = rightMenu.addItem("Home", null, null);        
+        MenuItem helpMenuItem = rightMenu.addItem("Help & Information", null, null);
+        MenuItem myaccountMenuItem = rightMenu.addItem("My Account", null, null);
+        myaccountMenuItem.addItem("Sign Out", null, menuCommand);
+        
+        headerTemplate.addComponent(menubar, "menubar");
+        headerTemplate.addComponent(rightMenu, "rightMenu");
+        
         layout.addComponent(headerTemplate);
     }
 
