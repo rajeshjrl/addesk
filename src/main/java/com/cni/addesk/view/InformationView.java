74fcccd77e5986036baae6034dff7eebf656b892
rajeshjrl <rajeshjrl@gmail.com>
2015-01-03 10:07:38 -0500

    Work in progress... Information page

--- src/main/java/com/cni/addesk/view/InformationView.java
+++ src/main/java/com/cni/addesk/view/InformationView.java
@@ -40,6 +40,7 @@ import com.vaadin.ui.ListSelect;
 import com.vaadin.ui.MenuBar;
 import com.vaadin.ui.PopupDateField;
 import com.vaadin.ui.Table;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Button.ClickEvent;
@@ -160,240 +161,93 @@ public class InformationView extends CustomComponent implements View{
 		informationHorizontalLayout.setSpacing(true);
 		informationHorizontalLayout.setSizeUndefined();
 		
+		Label informationNeededLabel = new Label("<span class='information_needed'>This information is needed in case<br/>we have questions about the creative.</span>", ContentMode.HTML);
+		informationHorizontalLayout.addComponent(informationNeededLabel);
+		
 		rootInformationHorizontalLayout.addComponent(informationHorizontalLayout);
 		rootInformationHorizontalLayout.addComponent(contactHorizontalLayout);
-		rootInformationHorizontalLayout.setComponentAlignment(contactHorizontalLayout, Alignment.TOP_RIGHT);		
+		rootInformationHorizontalLayout.setComponentAlignment(contactHorizontalLayout, Alignment.TOP_RIGHT);	
+		
+		Label contactLineLabel = new Label(Messages.getString("InformationView.pageTitleLineLabel"), ContentMode.HTML); 
+		
+		rootVerticalLayout.addComponent(contactLineLabel);
 		
 		contactRootHorizontalLayout.addComponent(rootInformationHorizontalLayout);
 		
-		/*AbstractOrderedLayout rootPortalSelectionsHorizontalLayout = new HorizontalLayout();
-		rootPortalSelectionsHorizontalLayout.setSpacing(true);
-		rootPortalSelectionsHorizontalLayout.setWidth(Messages.getString("InformationView.rootPortalSelectionsHorizontalLayoutWidth"));  
-		rootPortalSelectionsHorizontalLayout.setHeight(Messages.getString("InformationView.rootPortalSelectionsHorizontalLayoutHeight"));  
+		AbstractOrderedLayout contactInformationVerticalLayout = new VerticalLayout();
+		informationHorizontalLayout.setSpacing(true);
+		informationHorizontalLayout.setSizeUndefined();
+		contactInformationVerticalLayout.setMargin(new MarginInfo(false, true, false, true));
 		
-		AbstractOrderedLayout contactHorizontalLayout = new HorizontalLayout(contactLabel, contactLink);
-		contactHorizontalLayout.setSpacing(true);
-		contactHorizontalLayout.setSizeUndefined();
+		Label contactInformationLabel = new Label("<span class='contact_information'>Contact Information</span><br/>This information is necessary to contact you if we have<br/>questions related to your submission.", ContentMode.HTML);
+		contactInformationVerticalLayout.addComponent(contactInformationLabel);
 		
-		AbstractOrderedLayout portalSelectionsHorizontalLayout = new HorizontalLayout();
-		portalSelectionsHorizontalLayout.setSpacing(true);
-		portalSelectionsHorizontalLayout.setSizeUndefined();
+		AbstractOrderedLayout contactInformationHorizontalLayout = new HorizontalLayout();
+		contactInformationHorizontalLayout.setSpacing(true);
+		contactInformationHorizontalLayout.setSizeUndefined();
 		
-		rootPortalSelectionsHorizontalLayout.addComponent(portalSelectionsHorizontalLayout);
-		rootPortalSelectionsHorizontalLayout.addComponent(contactHorizontalLayout);
-		rootPortalSelectionsHorizontalLayout.setComponentAlignment(contactHorizontalLayout, Alignment.TOP_RIGHT);		
+		TextField firstName = new TextField("First Name");
+		firstName.setImmediate(true);
+		firstName.setInputPrompt("Required");
+		firstName.setWidth("350px");  
+		TextField lastName = new TextField("Last Name");
+		lastName.setImmediate(true);
+		lastName.setInputPrompt("Required");
+		lastName.setWidth("350px");  
+		TextField emailAddress = new TextField("email Address");
+		emailAddress.setImmediate(true);
+		emailAddress.setInputPrompt("Required");
+		emailAddress.setWidth("350px");  
+		TextField phoneNumber = new TextField("Phone Number");
+		phoneNumber.setImmediate(true);
+		phoneNumber.setInputPrompt("Required");
+		phoneNumber.setWidth("350px");
 		
-		contactRootHorizontalLayout.addComponent(rootPortalSelectionsHorizontalLayout);
+		AbstractOrderedLayout lhsFieldsVerticalLayout = new VerticalLayout(firstName, lastName, emailAddress, phoneNumber);
+		lhsFieldsVerticalLayout.setSpacing(true);
+		lhsFieldsVerticalLayout.setSizeUndefined();
+		lhsFieldsVerticalLayout.setMargin(new MarginInfo(true, true, true, false));
 		
-		ComboBox viewComboBox = new ComboBox();
-		viewComboBox.setNullSelectionAllowed(false);
-		viewComboBox.setCaption(Messages.getString("InformationView.viewComboBoxCaption"));  
-		viewComboBox.addItem(Messages.getString("InformationView.viewComboBox1"));  
-		viewComboBox.addItem(Messages.getString("InformationView.viewComboBox2"));  
-		viewComboBox.addItem(Messages.getString("InformationView.viewComboBox3"));  
-		viewComboBox.addItem(Messages.getString("InformationView.viewComboBox4"));  
+		contactInformationHorizontalLayout.addComponent(lhsFieldsVerticalLayout);
 		
-		ComboBox sortComboBox = new ComboBox();
-		sortComboBox.setNullSelectionAllowed(false);
-		sortComboBox.setCaption(Messages.getString("InformationView.sortComboBoxCaption"));  
-		sortComboBox.addItem(Messages.getString("InformationView.sortComboBox1"));  
-		sortComboBox.addItem(Messages.getString("InformationView.sortComboBox2"));  
+		TextField companyName = new TextField("Your Company Name");
+		companyName.setImmediate(true);
+		companyName.setWidth("350px");
+		TextField advertiserName = new TextField("Advertiser Name(If Different than Your Comapany)");
+		advertiserName.setImmediate(true);
+		advertiserName.setWidth("350px");
 		
-		Button filterButton = new Button(Messages.getString("InformationView.filterButtonText"));  
+		AbstractOrderedLayout rhsFieldsVerticalLayout = new VerticalLayout(companyName, advertiserName);
+		rhsFieldsVerticalLayout.setSpacing(true);
+		rhsFieldsVerticalLayout.setSizeUndefined();
+		rhsFieldsVerticalLayout.setMargin(new MarginInfo(true, false, true, false));
 		
-		final Label filtersAppliedLabel = new Label(Messages.getString("InformationView.filtersAppliedLabel"), ContentMode.HTML);  
-		filtersAppliedLabel.setVisible(false);
+		contactInformationHorizontalLayout.addComponent(rhsFieldsVerticalLayout);
 		
-		filterButton.addClickListener(new Button.ClickListener() {
-
-            @Override
+		contactInformationVerticalLayout.addComponent(contactInformationHorizontalLayout);
+		
+		Button continueButton = new Button("CONTINUE");		
+		Button cancelButton = new Button("CANCEL");
+		cancelButton.addClickListener(new Button.ClickListener() {
             public void buttonClick(ClickEvent event) {
-            	
-            	// Create a sub-window and add it to the main window
-            	final Window subWindow = new Window();
-            	subWindow.setWidth(Messages.getString("InformationView.subWindowWidth"));  
-            	subWindow.setHeight(Messages.getString("InformationView.subWindowHeight"));  
-            	subWindow.setImmediate(true);
-            	subWindow.setModal(true);
-            	
-            	final AbstractOrderedLayout subWindowVerticalLayout = new VerticalLayout();
-            	subWindowVerticalLayout.setSizeUndefined();
-            	subWindowVerticalLayout.setMargin(true);
-            	
-            	Label campaignListFiltersLabel = new Label(Messages.getString("InformationView.campaignListFiltersLabel"), ContentMode.HTML);  
-            	subWindowVerticalLayout.addComponent(campaignListFiltersLabel);
-            	
-            	final ComboBox advertiserComboBox = new ComboBox();
-            	advertiserComboBox.setNullSelectionAllowed(false);
-            	advertiserComboBox.setCaption(Messages.getString("InformationView.advertiserComboBoxCaption"));  
-            	advertiserComboBox.addItem(Messages.getString("InformationView.advertiserComboBox1"));  
-            	subWindowVerticalLayout.addComponent(advertiserComboBox);
-            	
-            	Label dateRangeLabel = new Label(Messages.getString("InformationView.dateRangeLabel"));  
-            	subWindowVerticalLayout.addComponent(dateRangeLabel);
-            	
-            	final PopupDateField fromDate = new PopupDateField();
-            	// Set the prompt
-            	fromDate.setValue(new Date());            	        
-            	// Set width explicitly to accommodate the prompt
-            	fromDate.setWidth(Messages.getString("InformationView.fromDateWidth"));  
-            	
-            	final PopupDateField toDate = new PopupDateField();
-            	// Set the prompt
-            	toDate.setValue(new Date());            	        
-            	// Set width explicitly to accommodate the prompt
-            	toDate.setWidth(Messages.getString("InformationView.toDateWidth"));  
-            	
-            	AbstractOrderedLayout dateRangeHorizontalLayout = new HorizontalLayout(fromDate, toDate);
-            	dateRangeHorizontalLayout.setSizeFull();
-            	dateRangeHorizontalLayout.setSpacing(true);
-            	
-            	subWindowVerticalLayout.addComponent(dateRangeHorizontalLayout);
-            	
-            	final ComboBox campaignTypeComboBox = new ComboBox();
-            	campaignTypeComboBox.setNullSelectionAllowed(false);
-            	campaignTypeComboBox.setCaption(Messages.getString("InformationView.campaignTypeComboBoxCaption"));  
-            	campaignTypeComboBox.addItem(Messages.getString("InformationView.campaignTypeComboBox1"));  
-            	campaignTypeComboBox.addItem(Messages.getString("InformationView.campaignTypeComboBox2"));  
-            	campaignTypeComboBox.addItem(Messages.getString("InformationView.campaignTypeComboBox3"));  
-            	
-            	subWindowVerticalLayout.addComponent(campaignTypeComboBox);
-            	
-            	// Have a bean container to put the beans in
-                final BeanItemContainer<ProductsName> productsContainer =  new BeanItemContainer<ProductsName>(ProductsName.class);
-                
-                // Put data in it
-                productsContainer.addItem(new ProductsName(Messages.getString("InformationView.productsContainer1")));  
-                productsContainer.addItem(new ProductsName(Messages.getString("InformationView.productsContainer2")));  
-                productsContainer.addItem(new ProductsName(Messages.getString("InformationView.productsContainer3")));  
-                productsContainer.addItem(new ProductsName(Messages.getString("InformationView.productsContainer4")));  
-                productsContainer.addItem(new ProductsName(Messages.getString("InformationView.productsContainer5")));  
-                productsContainer.addItem(new ProductsName(Messages.getString("InformationView.productsContainer6")));  
-                productsContainer.addItem(new ProductsName(Messages.getString("InformationView.productsContainer7")));  
-                productsContainer.addItem(new ProductsName(Messages.getString("InformationView.productsContainer8")));  
-                productsContainer.addItem(new ProductsName(Messages.getString("InformationView.productsContainer9")));  
-                productsContainer.addItem(new ProductsName(Messages.getString("InformationView.productsContainer10")));  
-            	
-            	final ListSelect productsSelect = new ListSelect(Messages.getString("InformationView.productsSelectCaption"), productsContainer);  
-            	//productsSelect.setMultiSelect(true);
-            	// Enable null selection
-            	productsSelect.setNullSelectionAllowed(false);
-            	// Set the caption mode to read the caption directly
-                // from the 'name' property of the bean
-            	productsSelect.setItemCaptionMode(ListSelect.ItemCaptionMode.PROPERTY );
-            	productsSelect.setItemCaptionPropertyId(Messages.getString("InformationView.itemCaptionPropertyId"));  
-            	
-            	productsSelect.addValueChangeListener(new Property.ValueChangeListener() {
-    		        public void valueChange(ValueChangeEvent event) {
-    		            if (event.getProperty().getValue() != null){
-    		                // Some feedback
-    		            	ProductsName productsName = (ProductsName)event.getProperty().getValue();
-    		            	productsContainer.removeItem(productsName);
-    		            	productsContainer.addItemAt(0, new ProductsName(productsName.getName()));
-    		            }
-    		        }
-    		    });
-            	
-            	subWindowVerticalLayout.addComponent(productsSelect);
-            	
-            	Button subWindowApplyButton = new Button(Messages.getString("InformationView.subWindowApplyButtonText"));  
-            	Button subWindowCancelButton = new Button(Messages.getString("InformationView.subWindowCancelButtonText"));  
-            	Button subWindowClearButton = new Button(Messages.getString("InformationView.subWindowClearButtonText"));  
-            	
-            	subWindowApplyButton.addClickListener(new ClickListener() {
-                    public void buttonClick(ClickEvent event) {
-                    	filtersAppliedLabel.setVisible(true);
-                    	subWindow.close(); // Close the sub-window
-                    }
-                });
-            	
-            	subWindowCancelButton.addClickListener(new ClickListener() {
-                    public void buttonClick(ClickEvent event) {
-                    	subWindow.close(); // Close the sub-window
-                    }
-                });
-            	
-            	subWindowClearButton.addClickListener(new ClickListener() {
-                    public void buttonClick(ClickEvent event) {
-                    	advertiserComboBox.setValue(null);
-                    	fromDate.setValue(new Date());
-                    	toDate.setValue(new Date());
-                    	campaignTypeComboBox.setValue(null);
-                    }
-                });
-            	
-            	AbstractOrderedLayout subWindowButtonsHorizontalLayout = new HorizontalLayout(subWindowApplyButton, subWindowCancelButton, subWindowClearButton);
-            	subWindowButtonsHorizontalLayout.setSizeFull();
-            	subWindowButtonsHorizontalLayout.setSpacing(true);
-            	subWindowButtonsHorizontalLayout.setMargin(new MarginInfo(true, false, false, false));
-            	
-            	subWindowVerticalLayout.addComponent(subWindowButtonsHorizontalLayout);
-            	
-            	subWindow.setContent(subWindowVerticalLayout);
-            	UI.getCurrent().addWindow(subWindow);
-            	
+            	// "Logout" the user
+	            getSession().setAttribute(Messages.getString("InformationView.sessionUser"), null);   
+
+	            // Refresh this view, should redirect to login view
+	            getUI().getNavigator().navigateTo(NAME);
             }
-            
-		});
+        });
 		
-		portalSelectionsHorizontalLayout.addComponent(viewComboBox);
-		portalSelectionsHorizontalLayout.addComponent(sortComboBox);
-		portalSelectionsHorizontalLayout.addComponent(filterButton);
-		portalSelectionsHorizontalLayout.setComponentAlignment(filterButton, Alignment.BOTTOM_LEFT);
-    	portalSelectionsHorizontalLayout.addComponent(filtersAppliedLabel);
-    	portalSelectionsHorizontalLayout.setComponentAlignment(filtersAppliedLabel, Alignment.BOTTOM_LEFT);
-		
-		AbstractOrderedLayout uploadCreativeHorizontalLayout = new HorizontalLayout();
-		uploadCreativeHorizontalLayout.setSizeUndefined();
-		uploadCreativeHorizontalLayout.setMargin(true);
-		
-		Embedded uploadImage = new Embedded(null, new ThemeResource(Messages.getString("InformationView.uploadImage")));  
-		
-		ClickableLabel uploadCreativeLabel = new ClickableLabel(Messages.getString("InformationView.uploadCreativeLabel"));  
-		
-		uploadCreativeHorizontalLayout.addComponent(uploadImage);
-		uploadCreativeHorizontalLayout.addComponent(uploadCreativeLabel);
-		uploadCreativeHorizontalLayout.setComponentAlignment(uploadCreativeLabel, Alignment.MIDDLE_LEFT);		
-		
-		rootVerticalLayout.addComponent(uploadCreativeHorizontalLayout);
-		
-		final ComboBox iWantToComboBox = new ComboBox(Messages.getString("InformationView.iWantToComboBoxCaption"));  
-        iWantToComboBox.setInvalidAllowed(false);
-        iWantToComboBox.setNullSelectionAllowed(false);
-        iWantToComboBox.setInputPrompt(Messages.getString("InformationView.iWantToComboBoxPrompt"));  
-        iWantToComboBox.addItem(Messages.getString("InformationView.iWantToComboBox1"));  
-        iWantToComboBox.addItem(Messages.getString("InformationView.iWantToComboBox2"));  
-        iWantToComboBox.addItem(Messages.getString("InformationView.iWantToComboBox3"));  
-        iWantToComboBox.addItem(Messages.getString("InformationView.iWantToComboBox4"));  
+		AbstractOrderedLayout buttonsHorizontalLayout = new HorizontalLayout(continueButton, cancelButton);
+		buttonsHorizontalLayout.setSizeUndefined();
+		buttonsHorizontalLayout.setSpacing(true);
+		buttonsHorizontalLayout.setMargin(new MarginInfo(true, false, true, false));
         
-        Label appearsLabel = new Label(Messages.getString("InformationView.appearsLabel"), ContentMode.HTML);  
-        Label detailsLabel = new Label(Messages.getString("InformationView.detailsLabel"), ContentMode.HTML);  
-        Label noticesLabel = new Label(Messages.getString("InformationView.noticesLabel"), ContentMode.HTML);  
+        contactInformationVerticalLayout.addComponent(buttonsHorizontalLayout);
 		
-		final Table activityTable = new Table();
-		activityTable.setHeight(Messages.getString("InformationView.activityTableHeight"));  
-		activityTable.addContainerProperty(Messages.getString("InformationView.activityTableColumn1"), Label.class, null);  
-		activityTable.addContainerProperty(Messages.getString("InformationView.activityTableColumn2"), Label.class, null);  
-		activityTable.addContainerProperty(Messages.getString("InformationView.activityTableColumn3"), Label.class, null);  
-		activityTable.addContainerProperty(Messages.getString("InformationView.activityTableColumn4"), ComboBox.class, null);  
-        
-        List<Object> iWantToList = new CopyOnWriteArrayList<Object>();
-        iWantToList.add(appearsLabel);
-        iWantToList.add(detailsLabel);
-        iWantToList.add(noticesLabel);
-        iWantToList.add(iWantToComboBox);
+		rootVerticalLayout.addComponent(contactInformationVerticalLayout);
 		
-		Object[] iWantToArray = iWantToList.toArray(new Object[iWantToList.size()]);
-		activityTable.addItem(iWantToArray, null);
 		
-		Label numberOfRowsLabel = new Label(Messages.getString("InformationView.numberOfRowsLabel1") + activityTable.size() + Messages.getString("InformationView.numberOfRowsLabel2"), ContentMode.HTML);  
-		
-		AbstractOrderedLayout tableVerticalLayout = new VerticalLayout();
-		tableVerticalLayout.setMargin(new MarginInfo(false, true, false, true));
-		tableVerticalLayout.setSizeUndefined();
-		tableVerticalLayout.addComponent(numberOfRowsLabel);
-		tableVerticalLayout.addComponent(activityTable);
-		rootVerticalLayout.addComponent(tableVerticalLayout);*/
 		
 	}
 
